e3f81fd58e09a6add8121576059c08b257baabce
Alex Cooksey <aecooksey2651@gmail.com>
2022-09-22 11:05:40 -0400

    Add about page text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div className="about-page">A</div>
-  )
+    return (
+        <div className="about-page">
+            <div id="bio">
+                <h4>Welcome, everyone! My name is Alex Cooksey, I'm a full-stack software engineer.</h4>
+                
+                <p>
+                    For nearly 10 years, I've relished the opportunity to help busy professionals improve their health and well-being as a personal trainer and nutrition coach. Wanting my career to positively touch the lives of (many!) more people, I made the decision last year to pivot into software engineering. In November 2021, I enrolled in the Flatiron School full-stack software engineering program. I appreciate that engineering feels less like a job and more like a set of tools that can be applied to solving complex and meaningful problems in innovative ways and at tremendous scale. Please head over to the Technical Projects page to check out some of the recent projects I've worked on.
+                </p>
+                    
+                <p>
+                    In my next role, I'd love to continue to develop and implement strategies that help people lead happier, healthier lives. I'm also deeply passionate about animal protection and, more broadly, sustainability. I can think of no better use of my skills than to find creative solutions that will ensure a better future for our planet and everyone on it. 
+                </p>
+                
+                <p>
+                    I continue to operate a small, remote personal training business - if you'd like to learn more, please visit the Coaching page, or simply message me at <a href="acooksey.hp@gmail.com">acooksey.hp@gmail.com</a>. In my spare time, I enjoy working out, reading, cooking, playing piano, and spending time in nature. 
+                </p>
+            </div>
+            <div id="fun-facts">
+                <ul>
+                    <li>Born and raised near Denver, CO; East Coast transplant since 2008</li>
+                    <li>Lifelong hero: Steve Irwin. I love animals...all of them. Don't ask me to kill a fly, I don't want to.</li>
+                    <li>Wanted to be a zoologist of some sort when I was young. Then I fell in love with playing piano...then math and chemistry. I Majored in East Asian Studies at Princeton, so naturally I became a personal trainer. Now I'm a software engineer. Life is full of surprises!</li>
+                    <li>I have perfect pitch - it hasn't carried over to singing ability. At all.</li>
+                    <li>I have a hard time showing my goofy side; I sometimes come across a little bit cerebral or serious. My sense of humor is silly with a dash of crazy - Jim Carrey and Robin Williams crack me up.</li>
+                    <li>Love cooking and eating food from all around the world - I'm down to try anything. Some favorites: rack of lamb, ramen (the legit stuff, but instant is great too), and all different sorts of curries...Jamaican curry goat might be the winner. Or Japanese katsu curry with omurice. Or the incredible crab curry I had at Kalaya in Philly (best Thai food I've ever had!). I could happily live off of Chongqing Xiao Mian, the cheap and delicious street noodles in the city where Jenny's family is from. Oh, and peanut butter. Peanut butter is life. Ok, yeah, I love food.</li>
+                    <li>I don't intrinsically enjoy working out <em>that</em> much, most of the time. Still, heavy lifting is therapy for me. Especially deadlifting.</li>
+                    <li>I still enjoy playing the Gen 1 & Gen 2 Pokemon video games. Pokemon Yellow was the first video game I owned, not getting rid of it anytime soon.</li>
+                    <li></li>
+                    <li>Among the countless things I have to be grateful for in this life, none matters more than the love of my life, Jenny Liu. Besides being a total boss, she's as kind and authentic a person as you're likely to meet in this world. We live in Jersey City, NJ with our cat, Piglet (aka Babe/Booboo), and dog, Sadie. You wouldn't think it possible, but I think he likes eating even more than we do.</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default About
