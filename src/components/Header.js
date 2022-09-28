b966af37931fa0f34048bfd787828b0d85b308e2
Alex Cooksey <aecooksey2651@gmail.com>
2022-09-28 13:31:58 -0400

    Work on navbar styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,26 +3,17 @@ import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
   return (
-    <div className="main-header">
-        <Nav justify variant="tabs" id="main-header__nav" className="justify-content-end" activeKey={1} >
-            <Nav.Item>
-                <Nav.Link className="main-header__item" eventKey={1} href="/projects">Portfolio</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey={2} href="/coaching" className="main-header__item">Coaching</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey={3} variant="light" className="main-header__item" href="/">Bio</Nav.Link>
-            </Nav.Item>
-            
-            
-            {/* <ul className="main-header__item-list">
-                <li className="main-header__item"><a href="/projects">Portfolio</a></li>
-                <li className="main-header__item"><a href="/coaching">Coaching</a></li>
-                <li className="main-header__item"><a href="/">About</a></li>
-            </ul> */}
+    <Navbar className="main-header" variant="dark">
+      <Container>
+        <Navbar.Brand className="main-header__logo"><code>{`< AlexCooksey />`}</code> </Navbar.Brand>
+        <Nav align="end" className="ml-auto">
+          <Nav.Link className="main-header__nav-link" href="/projects">Portfolio</Nav.Link>
+          <Nav.Link href="/coaching" className="main-header__nav-link">Coaching</Nav.Link>
+          <Nav.Link variant="light" className="main-header__nav-link" href="/">Bio</Nav.Link>
         </Nav>
-    </div>
+      </Container>
+    </Navbar>
+
   )
 }
 
