892d869408352a0e5f75109e7fcd8316b87dc49a
Alex Cooksey <aecooksey2651@gmail.com>
2022-09-29 09:10:11 -0400

    Change page coloring

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,64 +7,67 @@ import icelandImg from '../resources/images/iceland.jpg'
 
 const About = () => {
     return (
-        <Container className="about-page">
-            <h5 id="about-page__header">Hi, my name is Alex Cooksey. Welcome to my page!</h5>
-            <Container id="about-page__bio">
-                <div id="about-page__bio-text">
+        <div id="bio">
+            <Container className="about-page">
+                <h5 id="about-page__header">Hi, my name is Alex Cooksey. Welcome to my page!</h5>
+                <Container id="about-page__bio">
+                    <div id="about-page__bio-text">
+                        <p>
+                            For nearly 10 years, I've relished the opportunity to help busy professionals improve their health and well-being as a personal trainer and nutrition coach. Wanting my career to positively touch the lives of (many!) more people, I made the decision last year to pivot into software engineering.
+                        </p>
+                        <p>
+                            In November 2021, I enrolled in the Flatiron School full-stack software engineering program. I appreciate that engineering feels less like a job and more like a set of tools that can be applied to solving complex and meaningful problems in innovative ways and at tremendous scale. Please head over to the <a href="/projects">Portfolio</a> page to check out some of the recent projects I've worked on!
+                        </p>
+                    </div>
+                    <Container id="profile-image-div">
+                        <img src={profilePic} alt="profile-picture" id="profile-image" />
+                    </Container>
+                </Container>
+                <Container id="about-page__bio-text-pt2">
                     <p>
-                        For nearly 10 years, I've relished the opportunity to help busy professionals improve their health and well-being as a personal trainer and nutrition coach. Wanting my career to positively touch the lives of (many!) more people, I made the decision last year to pivot into software engineering.
+                        In my next role, I'd love to continue developing and implementing strategies that help people lead happier, healthier lives. I'm also deeply passionate about animal protection and, more broadly, sustainability. I can think of no better use of my skills than to find creative solutions that will ensure a better future for our planet and everyone on it.
                     </p>
+
                     <p>
-                        In November 2021, I enrolled in the Flatiron School full-stack software engineering program. I appreciate that engineering feels less like a job and more like a set of tools that can be applied to solving complex and meaningful problems in innovative ways and at tremendous scale. Please head over to the <a href="/projects">Portfolio</a> page to check out some of the recent projects I've worked on!
+                        I continue to operate a small, (mostly) remote personal training business, Coaching With Cooksey. If you'd like to work together or want to learn more, please visit the <a href="/coaching">Coaching</a> page. In my spare time, I enjoy working out, reading, cooking, playing piano, and spending time in nature.
                     </p>
+                </Container>
+
+                <hr />
+
+                <div id="fun-facts">
+                    <h3 id="fun-facts__header">Fun Facts!</h3>
+                    <Container id="fun-facts-pt1">
+                        <Container id="noodle-image-div">
+                            <img src={noodleImg} alt="chongqing-noodle-image" id="chongqing-noodle-img" />
+                        </Container>
+                        <ul id="fun-facts-list-pt1">
+                            <li>Born and raised near Denver, CO; East Coast transplant since 2008.</li>
+                            <li>Steve Irwin is a lifelong hero of mine. I love animals...all of them. Don't ask me to kill a fly, I don't want to.</li>
+                            <li>Wanted to be a zoologist of some sort when I was young. Then I fell in love with playing piano...then math and chemistry. I majored in East Asian Studies at Princeton, so naturally I became a personal trainer. Now I'm a software engineer. Life is full of surprises!</li>
+                            <li>I have perfect pitch - it hasn't carried over to singing ability. At all.</li>
+                            <li>I have a hard time showing my goofy side, which means I sometimes come across a little bit cerebral or serious. Just takes me some time to open up. My sense of humor is silly with a dash of crazy - Jim Carrey and Robin Williams crack me up.</li>
+                        </ul>
+                    </Container>
+                    <Container id="fun-facts-middle">
+                        <ul>
+                            <li>Love cooking and eating food from all around the world - I'm down to try anything. Some favorites: rack of lamb, ramen (the legit stuff, but instant is great too), and all different sorts of curries...Jamaican curry goat might be the winner. Or Japanese katsu curry with omurice. Or the incredible crab curry I had at Kalaya in Philly (best Thai food I've ever had!). I could happily live off of Chongqing Xiao Mian, the cheap and delicious street noodles in the city where Jenny's family is from. Oh, and peanut butter. Peanut butter is life. Ok, yeah, I love food.</li>
+                        </ul>
+                    </Container>
+                    <Container id="fun-facts-pt2">
+                        <ul>
+                            <li>I don't intrinsically enjoy working out <em>that</em> much, most of the time. Still, heavy lifting is therapy for me. Especially deadlifting.</li>
+                            <li>I still enjoy playing the Gen 1 & Gen 2 Pokemon video games. Pokemon Yellow was the first video game I owned, and I don't plan on getting rid of it anytime soon.</li>
+                            <li>Among the countless things I have to be grateful for in this life, none matters more than the love of my life, Jenny Liu. Besides being a total boss, she's as kind and authentic a person as you're likely to meet in this world. We live in Jersey City, NJ with our cat, Piglet (aka Babe/Booboo), and dog, Sadie. It hardly seems possible, but I think they like eating even more than we do.</li>
+                        </ul>
+                        <Container id="iceland-image-div">
+                            <img src={icelandImg} alt="iceland-image" id="iceland-img" />
+                        </Container>
+                    </Container>
                 </div>
-                <Container id="profile-image-div">
-                    <img src={profilePic} alt="profile-picture" id="profile-image" />
-                </Container>
-            </Container>
-            <Container id="about-page__bio-text-pt2">
-                <p>
-                    In my next role, I'd love to continue developing and implementing strategies that help people lead happier, healthier lives. I'm also deeply passionate about animal protection and, more broadly, sustainability. I can think of no better use of my skills than to find creative solutions that will ensure a better future for our planet and everyone on it.
-                </p>
+            </Container >
+        </div>
 
-                <p>
-                    I continue to operate a small, (mostly) remote personal training business, Coaching With Cooksey. If you'd like to work together or want to learn more, please visit the <a href="/coaching">Coaching</a> page. In my spare time, I enjoy working out, reading, cooking, playing piano, and spending time in nature.
-                </p>
-            </Container>
-
-            <hr />
-
-            <div id="fun-facts">
-                <h3 id="fun-facts__header">Fun Facts!</h3>
-                <Container id="fun-facts-pt1">
-                    <Container id="noodle-image-div">
-                        <img src={noodleImg} alt="chongqing-noodle-image" id="chongqing-noodle-img" />
-                    </Container>
-                    <ul id="fun-facts-list-pt1">
-                        <li>Born and raised near Denver, CO; East Coast transplant since 2008.</li>
-                        <li>Steve Irwin is a lifelong hero of mine. I love animals...all of them. Don't ask me to kill a fly, I don't want to.</li>
-                        <li>Wanted to be a zoologist of some sort when I was young. Then I fell in love with playing piano...then math and chemistry. I majored in East Asian Studies at Princeton, so naturally I became a personal trainer. Now I'm a software engineer. Life is full of surprises!</li>
-                        <li>I have perfect pitch - it hasn't carried over to singing ability. At all.</li>
-                        <li>I have a hard time showing my goofy side, which means I sometimes come across a little bit cerebral or serious. Just takes me some time to open up. My sense of humor is silly with a dash of crazy - Jim Carrey and Robin Williams crack me up.</li>
-                    </ul>
-                </Container>
-                <Container id="fun-facts-middle">
-                    <ul>
-                        <li>Love cooking and eating food from all around the world - I'm down to try anything. Some favorites: rack of lamb, ramen (the legit stuff, but instant is great too), and all different sorts of curries...Jamaican curry goat might be the winner. Or Japanese katsu curry with omurice. Or the incredible crab curry I had at Kalaya in Philly (best Thai food I've ever had!). I could happily live off of Chongqing Xiao Mian, the cheap and delicious street noodles in the city where Jenny's family is from. Oh, and peanut butter. Peanut butter is life. Ok, yeah, I love food.</li>
-                    </ul>
-                </Container>
-                <Container id="fun-facts-pt2">
-                    <ul>
-                        <li>I don't intrinsically enjoy working out <em>that</em> much, most of the time. Still, heavy lifting is therapy for me. Especially deadlifting.</li>
-                        <li>I still enjoy playing the Gen 1 & Gen 2 Pokemon video games. Pokemon Yellow was the first video game I owned, and I don't plan on getting rid of it anytime soon.</li>
-                        <li>Among the countless things I have to be grateful for in this life, none matters more than the love of my life, Jenny Liu. Besides being a total boss, she's as kind and authentic a person as you're likely to meet in this world. We live in Jersey City, NJ with our cat, Piglet (aka Babe/Booboo), and dog, Sadie. It hardly seems possible, but I think they like eating even more than we do.</li>
-                    </ul>
-                    <Container id="iceland-image-div">
-                        <img src={icelandImg} alt="iceland-image" id="iceland-img" />
-                    </Container>
-                </Container>
-            </div>
-        </Container >
     )
 }
 
--- src/pages/Coaching.js
+++ src/pages/Coaching.js
@@ -5,125 +5,127 @@ import alexKB from '../resources/images/AlexKB.png'
 
 const Coaching = () => {
   return (
-    <Container id="coaching-page">
-      <Container id="coaching-page__bio">
-        <img src={coachingLogo} alt="coaching with cooksey logo" id="cwc-logo" />
-        <div id="coaching-page__bio-header">
-          <h1>Coaching With Cooksey</h1>
-          <h6>NSCA-CPT, StrongFirst Elite Instructor, Precision Nutrition Level 2 Certified Nutrition Coach</h6>
-        </div>
-      </Container>
-      <Container>
-        <div id="coaching-page__bio-text">
-          <p>Interested in working together to improve your fitness and well-being? You've come to the right place!</p>
+    <div id="coaching">
+      <Container id="coaching-page">
+        <Container id="coaching-page__bio">
+          <img src={coachingLogo} alt="coaching with cooksey logo" id="cwc-logo" />
+          <div id="coaching-page__bio-header">
+            <h1>Coaching With Cooksey</h1>
+            <h6>NSCA-CPT, StrongFirst Elite Instructor, Precision Nutrition Level 2 Certified Nutrition Coach</h6>
+          </div>
+        </Container>
+        <Container>
+          <div id="coaching-page__bio-text">
+            <p>Interested in working together to improve your fitness and well-being? You've come to the right place!</p>
 
-          <p>I started my training career in 2013 at Equinox, where I ultimately had the pleasure of serving as a Tier X Coach and Master Instructor for fellow trainers and coaches. In 2020, I founded Coaching With Cooksey to help my clients develop the skills and habits necessary to lead healthier lives on their own terms. My clients' goals range from losing fat and building muscle to injury rehabilitation and ultramarathon training. Personally, I enjoy a nice mix of heavy barbell and kettlebell work alongside a healthy dose of good ol' bodybuilding training.</p>
+            <p>I started my training career in 2013 at Equinox, where I ultimately had the pleasure of serving as a Tier X Coach and Master Instructor for fellow trainers and coaches. In 2020, I founded Coaching With Cooksey to help my clients develop the skills and habits necessary to lead healthier lives on their own terms. My clients' goals range from losing fat and building muscle to injury rehabilitation and ultramarathon training. Personally, I enjoy a nice mix of heavy barbell and kettlebell work alongside a healthy dose of good ol' bodybuilding training.</p>
 
-          <p>Currently, I can be found working out at <a href="https://www.ironboundperformance.com">Ironbound Performance Athletics</a>, where I also coach a couple classes each week. It's a fantastic, tight-knit community; the fact that I've gotten in much better shape since joining just feels like a bonus. Come visit us in Jersey City, NJ!</p>
-        </div>
-      </Container>
+            <p>Currently, I can be found working out at <a href="https://www.ironboundperformance.com">Ironbound Performance Athletics</a>, where I also coach a couple classes each week. It's a fantastic, tight-knit community; the fact that I've gotten in much better shape since joining just feels like a bonus. Come visit us in Jersey City, NJ!</p>
+          </div>
+        </Container>
 
 
-      <hr />
+        <hr />
 
-      <Container id="coaching-services-section">
-        <div id="coaching-services-header">
-          <h2 id="coaching-services-header-h2">Services</h2>
-          <p><b>Please send all training inquiries to <a href="mailto:acooksey.hp@gmail.com?subject=Coaching With Cooksey Personal Training Inquiry">acooksey.hp@gmail.com</a>.</b></p>
-        </div>
+        <Container id="coaching-services-section">
+          <div id="coaching-services-header">
+            <h2 id="coaching-services-header-h2">Services</h2>
+            <p><b>Please send all training inquiries to <a href="mailto:acooksey.hp@gmail.com?subject=Coaching With Cooksey Personal Training Inquiry">acooksey.hp@gmail.com</a>.</b></p>
+          </div>
 
-        <Container id="coaching-service-cards">
-          <Card id="online-programming-card" className="coaching-service-card">
-            <div className="coaching-service-card__top">
-              <div className="coaching-service-card__header">
-                <h3>Online Programming</h3>
-              </div>
-              <p>Would you like help putting together a training program that makes sense for your needs? I’ve got you covered. Fully individualized training programs based on your goals, equipment, and available time to train along with text/email check-ins and monthly video calls.</p>
-            </div>
-
-            <Container className="coaching-service-pricing">
-              <h5>Pricing:</h5>
-              <ul>
-                <li>4-week program: $275</li>
-                <li>12-week program: $750 ($250/4 weeks)</li>
-              </ul>
-            </Container>
-          </Card>
-
-          <Card id="online-training-card" className="coaching-service-card">
-            <div className="coaching-service-card__top">
-              <div className="coaching-service-card__header">
-                <h3>Online Training</h3>
+          <Container id="coaching-service-cards">
+            <Card id="online-programming-card" className="coaching-service-card">
+              <div className="coaching-service-card__top">
+                <div className="coaching-service-card__header">
+                  <h3>Online Programming</h3>
+                </div>
+                <p>Would you like help putting together a training program that makes sense for your needs? I’ve got you covered. Fully individualized training programs based on your goals, equipment, and available time to train along with text/email check-ins and monthly video calls.</p>
               </div>
 
-              <p>Live, one-on-one training from the comfort of your living room, hotel room, or anywhere else you bring your screen. Progressive, customized training based on your goals, available equipment, and space.</p>
-            </div>
+              <Container className="coaching-service-pricing">
+                <h5>Pricing:</h5>
+                <ul>
+                  <li>4-week program: $275</li>
+                  <li>12-week program: $750 ($250/4 weeks)</li>
+                </ul>
+              </Container>
+            </Card>
 
-            <Container className="coaching-service-pricing">
-              <h5>Pricing:</h5>
-              <p>Single Session: $125<br />Packages:</p>
-              <ul>
-                <li>4 Sessions: $440 ($110/session)</li>
-                <li>8 Sessions: $840 ($105/session)</li>
-                <li>12 Sessions: $1200 ($100/session)</li>
-              </ul>
-            </Container>
-          </Card>
+            <Card id="online-training-card" className="coaching-service-card">
+              <div className="coaching-service-card__top">
+                <div className="coaching-service-card__header">
+                  <h3>Online Training</h3>
+                </div>
 
-          <Card id="inperson-training-card" className="coaching-service-card">
-            <div className="coaching-service-card__top">
-              <div className="coaching-service-card__header">
-                <h3>In-Person Training</h3>
-                <h6><em>*Available in Jersey City, NJ</em></h6>
+                <p>Live, one-on-one training from the comfort of your living room, hotel room, or anywhere else you bring your screen. Progressive, customized training based on your goals, available equipment, and space.</p>
               </div>
-              <p>Looking for the personal touch? Maybe you just need the change of scenery or prefer the atmosphere of the gym. We can get together to perfect exercise technique, work around injuries or discomfort, or simply push a bit harder than you otherwise might.</p>
-            </div>
 
-            <Container className="coaching-service-pricing">
-              <h5>Pricing:</h5>
-              <p>Single Session: $140<br />Packages:</p>
-              <ul>
-                <li>4 Sessions: $520 ($130/session)</li>
-                <li>8 Sessions: $1000 ($125/session)</li>
-                <li>12 Sessions: $1440 ($120/session)</li>
-              </ul>
-            </Container>
-          </Card>
+              <Container className="coaching-service-pricing">
+                <h5>Pricing:</h5>
+                <p>Single Session: $125<br />Packages:</p>
+                <ul>
+                  <li>4 Sessions: $440 ($110/session)</li>
+                  <li>8 Sessions: $840 ($105/session)</li>
+                  <li>12 Sessions: $1200 ($100/session)</li>
+                </ul>
+              </Container>
+            </Card>
+
+            <Card id="inperson-training-card" className="coaching-service-card">
+              <div className="coaching-service-card__top">
+                <div className="coaching-service-card__header">
+                  <h3>In-Person Training</h3>
+                  <h6><em>*Available in Jersey City, NJ</em></h6>
+                </div>
+                <p>Looking for the personal touch? Maybe you just need the change of scenery or prefer the atmosphere of the gym. We can get together to perfect exercise technique, work around injuries or discomfort, or simply push a bit harder than you otherwise might.</p>
+              </div>
+
+              <Container className="coaching-service-pricing">
+                <h5>Pricing:</h5>
+                <p>Single Session: $140<br />Packages:</p>
+                <ul>
+                  <li>4 Sessions: $520 ($130/session)</li>
+                  <li>8 Sessions: $1000 ($125/session)</li>
+                  <li>12 Sessions: $1440 ($120/session)</li>
+                </ul>
+              </Container>
+            </Card>
+          </Container>
+        </Container>
+
+        <hr />
+
+        <Container id="alexKB-image-container">
+          <img src={alexKB} alt="alex-turkish-get-up" id="alexKB-image" />
+        </Container>
+
+        <Container id="coaching-philosophy-section">
+          <h2 id="coaching-philosophy-header">Coaching Philosophy</h2>
+
+          <h3 className="coaching-philosophy-section-header">You're In the Driver's Seat</h3>
+          <p>My aim is to simplify fitness for busy professionals through collaborative coaching relationships. I believe you understand yourself - what you like, the unique challenges you face, your priorities in life - better than I ever will.</p>
+          <p>I don’t want to imitate the clichéd drill sergeant who just tells you what to do. Instead, I’d like to offer my education and experience alongside your input as we work together toward your goals. </p>
+
+
+          <h3 className="coaching-philosophy-section-header">An Integrative Approach</h3>
+          <p>
+            It’s critical to spend time in introspection. Why are your goals important to you? How do they stack up relative to other priorities in your life? What systems do you have in place to manage your fitness aspirations alongside the other demands on your time and energy? The first step toward lasting change lies in developing clear and realistic goals that are genuinely meaningful to you.
+          </p>
+          <p>
+            We’ll promote sustainable progress by complementing your exercise with proper nutrition, adequate sleep, physical recovery, and stress management. Keep these elements in harmony and combine them with a positive, growth-oriented mindset, and you’ll be well on the way toward long-term success.
+          </p>
+
+          <h3 className="coaching-philosophy-section-header">Skill Development Over Quick Fixes</h3>
+          <p>
+            When trying to make decisions for your health and wellness, it’s easy to get overwhelmed and lose the forest for the trees. Which diet is best for losing body fat? What workout program is ideal for building muscle? We’ll work together to cut through the confusion and learn solid principles so you can put your best foot forward.
+          </p>
+
+          <p>
+            At the end of the day, most people don’t need complex solutions; they need help developing strategies to <em>execute the basics consistently</em>. Our focus will be on building skills and habits that help you care for your body and mind throughout your life.
+          </p>
         </Container>
       </Container>
-
-      <hr />
-
-      <Container id="alexKB-image-container">
-        <img src={alexKB} alt="alex-turkish-get-up" id="alexKB-image" />
-      </Container>
-
-      <Container id="coaching-philosophy-section">
-        <h2 id="coaching-philosophy-header">Coaching Philosophy</h2>
-
-        <h3 className="coaching-philosophy-section-header">You're In the Driver's Seat</h3>
-        <p>My aim is to simplify fitness for busy professionals through collaborative coaching relationships. I believe you understand yourself - what you like, the unique challenges you face, your priorities in life - better than I ever will.</p>
-        <p>I don’t want to imitate the clichéd drill sergeant who just tells you what to do. Instead, I’d like to offer my education and experience alongside your input as we work together toward your goals. </p>
-
-
-        <h3 className="coaching-philosophy-section-header">An Integrative Approach</h3>
-        <p>
-          It’s critical to spend time in introspection. Why are your goals important to you? How do they stack up relative to other priorities in your life? What systems do you have in place to manage your fitness aspirations alongside the other demands on your time and energy? The first step toward lasting change lies in developing clear and realistic goals that are genuinely meaningful to you.
-        </p>
-        <p>
-          We’ll promote sustainable progress by complementing your exercise with proper nutrition, adequate sleep, physical recovery, and stress management. Keep these elements in harmony and combine them with a positive, growth-oriented mindset, and you’ll be well on the way toward long-term success.
-        </p>
-
-        <h3 className="coaching-philosophy-section-header">Skill Development Over Quick Fixes</h3>
-        <p>
-          When trying to make decisions for your health and wellness, it’s easy to get overwhelmed and lose the forest for the trees. Which diet is best for losing body fat? What workout program is ideal for building muscle? We’ll work together to cut through the confusion and learn solid principles so you can put your best foot forward.
-        </p>
-
-        <p>
-          At the end of the day, most people don’t need complex solutions; they need help developing strategies to <em>execute the basics consistently</em>. Our focus will be on building skills and habits that help you care for your body and mind throughout your life.
-        </p>
-      </Container>
-    </Container>
+    </div>
   )
 }
 
